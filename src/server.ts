8d37564460e683fcc29b8f77ac6043ab513af398
Gabriel Sá <gabriel.sa@laura-br.com>
2021-05-10 22:04:03 -0300

    Changed env

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,9 @@ import { app } from "./app"
 
 dotenv.config()
 
+const PORT = parseInt(process.env.PORT || "3000")
+const HOST = "0.0.0.0"
+
 
 app.use(cors({ origin: process.env.APP_URL }))
-app.listen(3000, "0.0.0.0")
+app.listen(PORT, HOST)
